6410fa58ab4f0c8e54663db8e5e7370495e08260
lml43 <34567933+lml43@users.noreply.github.com>
2024-06-08 22:48:18 +0200

    github action

---
+++ src/main/java/com/altsol/sportcourt/SportcourtApplication.java
@@ -0,0 +1,13 @@
+package com.altsol.sportcourt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportcourtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportcourtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/altsol/sportcourt/SportcourtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.altsol.sportcourt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportcourtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
